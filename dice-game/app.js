fb5684e0530fbd048f8dd19fa7d5199d4f6d4c01
Tsendbaatar <tsendbaatar.o@gmail.com>
2020-05-18 10:40:25 +0800

    update

---
+++ dice-game/app.js
@@ -0,0 +1,49 @@
+// Toglogchiin eeljiig hadgalah huvisagch 1 toglogch 0, 2 toglogch 1
+var activePlayer = 0;
+
+//Toglogchdiin tsugluulsan onoog tsugluulah huvisagch
+var scores = [0, 0];
+
+//Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvisagch
+var roundScore = 0;
+//Shoonii ali talaaraa buusniig hadgalah huvisagch heregtei, 1-6 utgiig ene huvisagchid sanamsargui uusgeh
+
+//var dice1 = Math.floor(Math.random() * 6) + 1;
+//<div class="player-score" id="score-0">43</div>
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", shooshid);
+//console.log("Shoo :" + dice);
+
+function shooshid() {
+  //sanamsargui toog gargah
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  //buusan shoonii toog zurag deer gargah
+  diceDom.src = "dice-" + dice + ".png";
+
+  if (dice !== 1) {
+    roundScore = roundScore + dice;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    //idvehite toglogch ni 0 bval 1 ugui bol 0 bolgono
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //shoog tur alga bolgoh
+    diceDom.style.display = "none";
+  }
+}
